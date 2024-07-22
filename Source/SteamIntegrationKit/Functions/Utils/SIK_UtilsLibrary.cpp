7fa565bf677ae367fec33214af5178e6b9365162
Quentin MONTESSUIS <quentinmontessuis.pro@gmail.com>
2024-07-22 20:05:50 +0200

    Add OpenSteamKeyboard and IsControllerConnected functions/blueprint node

--- Source/SteamIntegrationKit/Functions/Utils/SIK_UtilsLibrary.cpp
+++ Source/SteamIntegrationKit/Functions/Utils/SIK_UtilsLibrary.cpp
@@ -1,8 +1,46 @@
 ﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
-
+// OpenSteamKeyboard & IsControllerConnected by @MarOwNFR
 
 #include "SIK_UtilsLibrary.h"
+#include <isteamcontroller.h>
+#include <isteaminput.h>
+#include <steam_api_common.h>
+#include <steamtypes.h>
 
+bool USIK_UtilsLibrary::IsControllerConnected()
+{
+
+	ISteamInput* SteamInputInterface = SteamInput();
+	if (!SteamInputInterface)
+	{
+		return false;
+	}
+
+	if (!SteamInputInterface->Init(false))
+	{
+		return false;
+	}
+
+	SteamInputInterface->RunFrame();
+
+	InputHandle_t handlesOut[STEAM_INPUT_MAX_COUNT] = { 0 };
+
+	int numControllers = SteamInputInterface->GetConnectedControllers(handlesOut);
+
+	if (numControllers > 0)
+	{
+		return true;
+	}
+
+	return false;
+}
+void USIK_UtilsLibrary::OpenSteamKeyboard(int32 KeyboardMode, int32 TextFieldXPosition, int32 TextFieldYPosition, int32 TextFieldWidth, int32 TextFieldHeight)
+{
+	if (SteamUtils())
+	{
+		SteamUtils()->ShowFloatingGamepadTextInput(static_cast<EFloatingGamepadTextInputMode>(KeyboardMode), TextFieldXPosition, TextFieldYPosition, TextFieldWidth, TextFieldHeight);
+	}
+}
 
 bool USIK_UtilsLibrary::OverlayNeedsPresent()
 {
@@ -82,9 +120,10 @@ bool USIK_UtilsLibrary::GetImageRGBA(int32 Image, TArray<FColor>& OutData, int32
 	// Assign the width and height to the output parameters
 	OutWidth = static_cast<int32>(Width);
 	OutHeight = static_cast<int32>(Height);
-	
+
 	return true;
 }
+
 
 FString USIK_UtilsLibrary::GetIPCountry()
 {
@@ -92,7 +131,7 @@ FString USIK_UtilsLibrary::GetIPCountry()
 	{
 		return FString();
 	}
-	return UTF8_TO_TCHAR(SteamUtils()->GetIPCountry());
+	return FString(UTF8_TO_TCHAR(SteamUtils()->GetIPCountry()));
 }
 
 int32 USIK_UtilsLibrary::GetSecondsSinceAppActive()
@@ -219,27 +258,6 @@ void USIK_UtilsLibrary::SetVRHeadsetStreamingEnabled(bool bEnabled)
 		return;
 	}
 	SteamUtils()->SetVRHeadsetStreamingEnabled(bEnabled);
-}
-
-bool USIK_UtilsLibrary::ShowGamepadTextInput(TEnumAsByte<ESIK_EGamepadTextInputMode> InputMode,
-	TEnumAsByte<ESIK_EGamepadTextInputLineMode> LineInputMode, const FString& Description, int32 CharMax,
-	const FString& ExistingText)
-{
-	if(!SteamUtils())
-	{
-		return false;
-	}
-	return SteamUtils()->ShowGamepadTextInput(static_cast<EGamepadTextInputMode>(InputMode.GetValue()), static_cast<EGamepadTextInputLineMode>(LineInputMode.GetValue()), TCHAR_TO_ANSI(*Description), CharMax, TCHAR_TO_ANSI(*ExistingText));
-}
-
-bool USIK_UtilsLibrary::ShowFloatingGamepadTextInput(TEnumAsByte<ESIK_EFloatingGamepadTextInputMode> KeyboardMode,
-	int32 TextFieldXPosition, int32 TextFieldYPosition, int32 TextFieldWidth, int32 TextFieldHeight)
-{
-	if(!SteamUtils())
-	{
-		return false;
-	}
-	return SteamUtils()->ShowFloatingGamepadTextInput(static_cast<EFloatingGamepadTextInputMode>(KeyboardMode.GetValue()), TextFieldXPosition, TextFieldYPosition, TextFieldWidth, TextFieldHeight);
 }
 
 void USIK_UtilsLibrary::StartVrDashboard()
